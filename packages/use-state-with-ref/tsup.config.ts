19409a77f828c54d545798b0c442bc9a383a469f
William Wong <compulim@users.noreply.github.com>
2024-05-29 18:49:19 -0700

    Move to tsup and remove named exports (#24)
    * Move to tsup and remove named exports
    
    * Move @babel/preset-react to devDependencies
    
    * Update entry
    
    * Update entry
    
    * Update entry
    
    * Simplify script

---
+++ packages/use-state-with-ref/tsup.config.ts
@@ -0,0 +1,12 @@
+import { defineConfig } from 'tsup';
+
+export default defineConfig([
+  {
+    dts: true,
+    entry: {
+      'use-state-with-ref': './src/index.ts'
+    },
+    format: ['cjs', 'esm'],
+    sourcemap: true
+  }
+]);
